1f220a4c6f9c4606a72f937b21b969ff77f4d7d7
Sejal Kumari <sejal.k6126@gmail.com>
2026-08-25 01:20:05 +0530

    axios installed and api folder created

---
+++ Project-3/frontend/src/api/authApi.js
@@ -0,0 +1,16 @@
+import api from "./axios";
+
+export const registerUser = async (userData) => {
+  const response = await api.post("/auth/register", userData);
+  return response.data;
+};
+
+export const loginUser = async (userData) => {
+  const response = await api.post("/auth/login", userData);
+  return response.data;
+};
+
+export const getCurrentUser = async () => {
+  const response = await api.get("/auth/me");
+  return response.data;
+};
